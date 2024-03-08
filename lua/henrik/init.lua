0db0fcfbba5081b5064d05aaa9faba3a5e1b2893
Hknots <hknots99@gmail.com>
2024-03-08 18:34:27 +0100

    configure fugitive & set standard mappings

---
+++ lua/henrik/init.lua
@@ -0,0 +1,4 @@
+require("henrik.standard")
+require("henrik.remap")
+require("henrik.lazy")
+require("henrik.theme")
---
+++ lua/henrik/remap.lua
@@ -0,0 +1,5 @@
+vim.keymap.set("n", "<leader>q", vim.cmd.Ex) 
+
+vim.keymap.set("n", "<leader>y", "\"+y<CR>")
+vim.keymap.set("v", "<leader>y", "\"+y<CR>")
+

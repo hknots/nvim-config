61bb1839b5887436dec0013a907d0269758ea7f5
Hknots <hknots99@gmail.com>
2024-03-08 22:28:49 +0100

    window navigation keybinds

--- lua/henrik/remap.lua
+++ lua/henrik/remap.lua
@@ -1,5 +1,10 @@
-vim.keymap.set("n", "<leader>q", vim.cmd.Ex) 
+vim.keymap.set("n", "<leader>q", ":q<CR>") 
 
 vim.keymap.set("n", "<leader>y", "\"+y<CR>")
 vim.keymap.set("v", "<leader>y", "\"+y<CR>")
+
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
 

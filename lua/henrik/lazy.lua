1c93b4177ae2e3c7e5c34f6f93a390916fee257e
Hknots <hknots99@gmail.com>
2024-03-09 19:21:03 +0100

    add telescope

--- lua/henrik/lazy.lua
+++ lua/henrik/lazy.lua
@@ -31,4 +31,8 @@ require("lazy").setup({
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' }
 	},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
 })

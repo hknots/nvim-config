ef8244e02b1ef17a59f9bd2aa1dcbd2aaa4b79e5
Hknots <hknots99@gmail.com>
2024-03-09 11:50:08 +0100

    add more git commands

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gw", vim.cmd.Gwrite)
+vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse)
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")

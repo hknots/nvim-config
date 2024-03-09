84093271a2b4d1e9e66aa134d2651a62d21f59b0
Hknots <hknots99@gmail.com>
2024-03-09 19:24:34 +0100

    install treesitter

--- lua/henrik/lazy.lua
+++ lua/henrik/lazy.lua
@@ -35,4 +35,8 @@ require("lazy").setup({
 		'nvim-telescope/telescope.nvim', tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' }
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 })

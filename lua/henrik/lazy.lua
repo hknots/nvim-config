bb4ee0714db64ec7b1555178b82ecd911f8babdd
Hknots <hknots99@gmail.com>
2024-03-09 20:43:22 +0100

    add basic lsp

--- lua/henrik/lazy.lua
+++ lua/henrik/lazy.lua
@@ -40,4 +40,12 @@ require("lazy").setup({
 		build = ":TSUpdate",
 	},
 	"mbbill/undotree",
+	-- Lsp
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
 })
